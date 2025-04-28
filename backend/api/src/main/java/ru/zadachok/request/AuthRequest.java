3c1f72e9b8e8c78af9367b03be068fb19be45dee
mihulich <proinisbro@gmail.com>
2025-04-28 08:45:33 +0300

    TPP-0 other: test

--- backend/api/src/main/java/ru/zadachok/request/AuthRequest.java
+++ backend/api/src/main/java/ru/zadachok/request/AuthRequest.java
@@ -1,5 +1,6 @@
 package ru.zadachok.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
@@ -10,9 +11,11 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequest {
+    @JsonProperty("login")  // Входящее поле в JSON будет "login"
     @NotBlank
     private String login;
 
+    @JsonProperty("password")  // Входящее поле в JSON будет "login"
     @NotBlank
     private String password;
 }
--- backend/api/src/main/java/ru/zadachok/request/CreateLobbyRequest.java
+++ backend/api/src/main/java/ru/zadachok/request/CreateLobbyRequest.java
@@ -1,6 +1,8 @@
 // dto/request/CreateLobbyRequest.java
 package ru.zadachok.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Data
@@ -8,5 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateLobbyRequest {
+    @JsonProperty("creatorID")  // Входящее поле в JSON будет "login"
+    @NotBlank
     private Integer creatorId;  // ID пользователя, инициирующего создание
 }
