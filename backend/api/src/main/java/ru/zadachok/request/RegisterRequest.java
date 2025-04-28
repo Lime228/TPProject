e73ba94f8af7acdacdb5dffc3b4f70a41c5e6d62
mihulich <proinisbro@gmail.com>
2025-04-28 08:29:34 +0300

    TPP-0 other: test

--- backend/api/src/main/java/ru/zadachok/request/RegisterRequest.java
+++ backend/api/src/main/java/ru/zadachok/request/RegisterRequest.java
@@ -1,25 +1,28 @@
 package ru.zadachok.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-
-
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+
+    @JsonProperty("login")  // Входящее поле в JSON будет "login"
     @NotBlank
     @Size(min = 3, max = 20)
     private String login;
 
+    @JsonProperty("password")  // Входящее поле в JSON будет "password"
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
 
+    @JsonProperty("customer_email")  // Тут ты меняешь отображение
     @NotBlank
     @Email
     private String customer_email;
