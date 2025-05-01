8d7e08134a4226dfdfda913ea117f71d2c083903
mihulich <proinisbro@gmail.com>
2025-05-01 13:48:36 +0300

    TPP-84 other: Product update

--- backend/api/src/main/java/ru/zadachok/request/UpdateProductRequest.java
+++ backend/api/src/main/java/ru/zadachok/request/UpdateProductRequest.java
@@ -3,9 +3,15 @@ package ru.zadachok.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateProductRequest {
     @JsonProperty("productid")  // Входящее поле в JSON будет "productid" (id продукта)
     @NotBlank
