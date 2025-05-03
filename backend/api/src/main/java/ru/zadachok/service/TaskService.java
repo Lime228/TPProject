f8465dc862286b03209652ffdedca308b0f7253e
jumpie <72701292+Lime228@users.noreply.github.com>
2025-05-03 22:37:28 +0300

    TPP-110 fix: update TaskService.java

--- backend/api/src/main/java/ru/zadachok/service/TaskService.java
+++ backend/api/src/main/java/ru/zadachok/service/TaskService.java
@@ -30,7 +30,7 @@ public class TaskService {
                 .startDate(request.getStartDate())
                 .endDate(request.getEndDate())
                 .isActive(true)
-                .customerId(request.getCustomerId)
+                .customerId(request.getCustomerId())
                 .build();
 
         Task savedTask = taskRepository.save(task);
