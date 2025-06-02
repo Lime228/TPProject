25bb7478d96c85664215b380f2c6f2d6f08069f5
Lime228 <Vladimir.Mosyakin@yandex.ru>
2025-06-02 12:33:25 +0300

    TPP-151 fix: add json property

--- backend/api/src/main/java/ru/zadachok/dto/AI/GenerationResponse.java
+++ backend/api/src/main/java/ru/zadachok/dto/AI/GenerationResponse.java
@@ -1,9 +1,12 @@
 package ru.zadachok.dto.AI;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class GenerationResponse {
+    @JsonProperty("generated_text")
     private String generatedText;
+    @JsonProperty("status")
     private String status;
 }
